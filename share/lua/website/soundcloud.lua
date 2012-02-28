53204fd1c5e3d4c715526a482c1693b59a9a3442
Toni Gundogdu <legatvs@gmail.com>
2012-02-28 23:07:11 +0200

    soundcloud.lua: Rework title parsing
    Remove character conversion which is an overkill and failed with some
    of the URLs (e.g. see the 2nd below). Instead, grab the title from og:title.
    
    Example URLs:
     * http://is.gd/CCiZvS
     * http://is.gd/Tv0huY

--- share/lua/website/soundcloud.lua
+++ share/lua/website/soundcloud.lua
@@ -18,11 +18,6 @@
 -- License along with this library; if not, write to the Free Software
 -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 -- 02110-1301  USA
---
--- For Soundcloud.toUtf8():
--- Copyright 2004 by Rici Lake. Permission is granted to use this code under
--- the same terms and conditions as found in the Lua copyright notice at
--- http://www.lua.org/license.html.
 
 local Soundcloud = {} -- Utility functions unique to this script
 
@@ -48,35 +43,30 @@ end
 
 -- Parse media URL.
 function parse(self)
-    self.host_id  = "soundcloud"
-
+    self.host_id = "soundcloud"
     Soundcloud.normalize(self)
 
-    local page = quvi.fetch(self.page_url)
+    local p = quvi.fetch(self.page_url)
+    local m = p:match("window%.SC%.bufferTracks%.push(%(.-%);)")
+                or error("no match: metadata")
 
-    local _,_,s = page:find("window%.SC%.bufferTracks%.push(%(.-%);)")
-    local metadata = s or error("no match: metadata")
+    self.id = m:match('"uid":"(%w-)"') or error("no match: media id")
 
-    local _,_,s = metadata:find('"uid":"(%w-)"')
-    self.id = s or error("no match: media id")
+    self.thumbnail_url = ''
+    self.title = nil -- Ugly but works.
+    for c,p in p:gfind('<meta content="(.-)" property="(.-)"') do
+        if p == 'og:title' then
+            self.title = c
+        elseif p == 'og:image' then
+            self.thumbnail_url = c
+        end
+    end
 
-    local _,_,s = metadata:find('"title":"(.-)"')
-    local title  = s or error("no match: media title")
-    -- Unescape the Unicode strings if any
-    -- the HTML will be unescaped by quvi itself
-    self.title = string.gsub(title, "\\u(%x+)",
-        function (h)
-            return Soundcloud.toUtf8(tonumber(h, 16))
-        end)
+    self.title = self.title or error("no match: media title")
+    self.duration = tonumber(m:match('"duration":(%d-),')) or 0
 
-    local _,_,s = page:find('content="([:/%w%?%.-]-)" property="og:image"')
-    self.thumbnail_url = s or ""
-
-    local _,_,s = metadata:find('"duration":(%d-),')
-    self.duration = tonumber(s) or 0
-
-    local _,_,s = metadata:find('"streamUrl":"(.-)"')
-    self.url  = { s }  or error("no match: stream URL")
+    local s  = m:match('"streamUrl":"(.-)"') or error("no match: media URL")
+    self.url = {s}
 
     return self
 end
@@ -95,30 +85,6 @@ function Soundcloud.normalize(self) -- "Normalize" an embedded URL
 
     local s = quvi.fetch(oe_url):match('href=\\"(.-)\\"')
     self.page_url = s or error('no match: page url')
-end
-
--- Adapted from http://luaparse.luaforge.net/libquery.lua.html
---
--- Convert an integer to a UTF-8 sequence, without checking for
--- invalid codes.
--- This originally had calls to floor scattered about but it is
--- not necessary: string.char does a "C" conversion from float to int,
--- which is a truncate towards zero operation; i must be non-negative,
--- so that is the same as floor.
-function Soundcloud.toUtf8(i)
-  if i <= 127 then return string.char(i)
-   elseif i <= tonumber("7FF", 16) then
-    return string.char(i / 64 + 192, math.mod(i, 64) + 128)
-   elseif i <= tonumber("FFFF", 16) then
-    return string.char(i / 4096 + 224,
-                   math.mod(i / 64, 64) + 128,
-                   math.mod(i, 64) + 128)
-   else
-    return string.char(i / 262144 + 240,
-                   math.mod(i / 4096, 64) + 128,
-                   math.mod(i / 64, 64) + 128,
-                   math.mod(i, 64) + 128)
-  end
 end
 
 -- vim: set ts=4 sw=4 tw=72 expandtab:
