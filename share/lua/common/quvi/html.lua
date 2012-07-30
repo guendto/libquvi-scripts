53a9fc02c7b881a4323b44bb8f0ec3c5c914ab34
Toni Gundogdu <legatvs@gmail.com>
2012-07-30 20:44:21 +0300

    quvi/html.lua: Apply new style
    Make minor style changes to the code

--- share/lua/common/quvi/html.lua
+++ share/lua/common/quvi/html.lua
@@ -1,4 +1,3 @@
-
 -- libquvi-scripts
 -- Copyright (C) 2011  Toni Gundogdu <legatvs@gmail.com>
 --
@@ -21,7 +20,7 @@
 --
 
 --
--- A modularized revision of <http://www.hpelbers.org/lua/utf8>
+-- A modularized version of <http://www.hpelbers.org/lua/utf8>
 --
 -- convert numeric html entities to utf8
 -- example:  &#8364;    ->   €
@@ -31,43 +30,43 @@ local M    = {}
 local char = string.char
 
 function M.tail(n, k)
-    local u, r=''
-    for i=1,k do
-        n,r = math.floor(n/0x40), n%0x40
-        u = char(r+0x80) .. u
-    end
-    return u, n
+  local u, r=''
+  for i=1,k do
+    n,r = math.floor(n/0x40), n%0x40
+    u = char(r+0x80) .. u
+  end
+  return u,n
 end
 
 function M.to_utf8(a)
-    local n, r, u = tonumber(a)
-    if n<0x80 then                        -- 1 byte
-        return char(n)
-    elseif n<0x800 then                   -- 2 byte
-        u, n = M.tail(n, 1)
-        return char(n+0xc0) .. u
-    elseif n<0x10000 then                 -- 3 byte
-        u, n = M.tail(n, 2)
-        return char(n+0xe0) .. u
-    elseif n<0x200000 then                -- 4 byte
-        u, n = M.tail(n, 3)
-        return char(n+0xf0) .. u
-    elseif n<0x4000000 then               -- 5 byte
-        u, n = M.tail(n, 4)
-        return char(n+0xf8) .. u
-    else                                  -- 6 byte
-        u, n = M.tail(n, 5)
-        return char(n+0xfc) .. u
-    end
+  local n, r, u = tonumber(a)
+  if n<0x80 then                        -- 1 byte
+    return char(n)
+  elseif n<0x800 then                   -- 2 byte
+    u,n = M.tail(n, 1)
+    return char(n+0xc0) .. u
+  elseif n<0x10000 then                 -- 3 byte
+    u,n = M.tail(n, 2)
+    return char(n+0xe0) .. u
+  elseif n<0x200000 then                -- 4 byte
+    u,n = M.tail(n, 3)
+    return char(n+0xf0) .. u
+  elseif n<0x4000000 then               -- 5 byte
+    u,n = M.tail(n, 4)
+    return char(n+0xf8) .. u
+  else                                  -- 6 byte
+    u,n = M.tail(n, 5)
+    return char(n+0xfc) .. u
+  end
 end
 
---
---for line in io.lines() do
---    out =  string.gsub(line, '&#(%d+);', to_utf8)
---    print(out)
---end
---
+--[[
+for s in io.lines() do
+  local o = s:gsub('&#(%d+);', M.to_utf8)
+  print(o)
+end
+]]--
 
 return M
 
--- vim: set ts=4 sw=4 tw=72 expandtab:
+-- vim: set ts=2 sw=2 tw=72 expandtab:
--- share/lua/common/quvi/util.lua
+++ share/lua/common/quvi/util.lua
@@ -1,6 +1,5 @@
-
 -- libquvi-scripts
--- Copyright (C) 2010-2011  Toni Gundogdu <legatvs@gmail.com>
+-- Copyright (C) 2010-2012  Toni Gundogdu <legatvs@gmail.com>
 --
 -- This file is part of libquvi-scripts <http://quvi.sourceforge.net/>.
 --
@@ -22,141 +21,192 @@
 
 local M = {}
 
--- http://www.lua.org/pil/20.3.html
-function M.decode (s)
-    r = {}
-    for n,v in s:gmatch ("([^&=]+)=([^&=]+)") do
-        n = M.unescape (n)
-        r[n] = v
+--[[
+Check whether a string A ends with string B.
+Parameters:
+  a .. String A
+  b .. String B
+Returns:
+  true if string A ends with string B.
+]]--
+function M.ends(a, b) -- http://lua-users.org/wiki/StringRecipes
+  return a:sub(-#b) == b
+end
+
+--[[
+Compare quality properties of two media entities. Compares the height, then
+the width, followed by the bitrate property comparison (if it is set).
+Parameters:
+  a .. Media entity A
+  b .. Media entity B
+Returns:
+  true if entity A is the higher quality, otherwise false.
+]]--
+function M.is_higher_quality(a, b)
+  if a.height > b.height then
+    if a.width > b.width then
+      if a['bitrate'] then -- Optional
+        if a.bitrate > b.bitrate then return true end
+      else
+        return true
+      end
     end
-    return r
+  end
+  return false
 end
 
--- http://www.lua.org/pil/20.3.html
-function M.unescape (s)
-    s = s:gsub ('+', ' ')
-    s = s:gsub ('%%(%x%x)', function (h)
-            return string.char (tonumber (h, 16))
-        end)
-    return s
-end
-
-function M.slash_unescape (s)
-    s = s:gsub ('\\(.)', '%1')
-    return s
-end
-
--- handles
---  Check whether a website script can "handle" the specified URL
--- Params:
---  url     .. video page URL
---  domains .. table of domain names
---  paths   .. table of URL path patterns to match
---  queries .. table of URL query patterns to match
-function M.handles(url, domains, paths, queries)
-    if not url or not domains then
-        return false
+--[[
+Compare quality properties of two media entities. Compares the height, then
+the width, followed by the bitrate property comparison (if it is set).
+Parameters:
+  a .. Media entity A
+  b .. Media entity B
+Returns:
+  true if entity A is the lower quality, otherwise false.
+]]--
+function M.is_lower_quality(a, b)
+  if a.height < b.height then
+    if a.width < b.width then
+      if a['bitrate'] then -- Optional
+        if a.bitrate < b.bitrate then return true end
+      else
+        return true
+      end
     end
-    local U = require 'quvi/url'
-    local t = U.parse(url)
---    for k,v in pairs(t) do print(k,v) end
-    local r = M.contains(t.host, domains)
-    if r then
-        if paths then
-            r = M.contains(t.path, paths)
-        end
-        if r then
-            if queries then
-                if t.query then
-                    r = M.contains(t.query, queries)
-                else
-                    r = false
-                end
-            end
-        end
-    end
-    return r
+  end
+  return false
 end
 
-function M.contains(s,t)
-    if not s then return false end
-    for k,v in pairs(t) do
-        if s:find(v) then return true end
+--[[
+Choose a media entity best matching the "requested format" which is string
+set by an application using libquvi API.
+Parameters:
+  qargs   .. Library args (containing .req_format)
+  t       .. Table containing the media properties (incl. media stream URLs)
+  cb_best .. Best format callback function
+  cb_def  .. Default format callback function
+  cb_to_s .. Format to string callback function
+Returns:
+  Chosen media entity.
+
+The req_format string may be an array of format strings (e.g.
+"foo,bar,baz").
+
+A format string may be:
+  - default .. Use whatever a media script determines the 'default'
+  - croak   .. Fail if nothing matched
+  - best    .. Whatever a media script determines to be the 'best'
+
+By default, the function falls back to 'default' format if nothing
+matches. To override this, 'croak' may be used to trigger an error
+instead.
+]]--
+function M.choose_format(qargs, t, cb_default, cb_best, cb_fmt_to_s)
+  for f in M.tokenize(qargs.req_format, '[%w-_]+') do
+    if     f == 'croak' then error('no match: requested format')
+    elseif f == 'best'  then return cb_best(t)
+    else
+      for _,v in pairs(t) do -- Match to user format string.
+        if f == cb_fmt_to_s(v) then return v end
+      end
     end
-    return false
+  end
+  return cb_default(t) -- Fall back to the 'default' format.
 end
 
-function M.ends(s,e) -- http://lua-users.org/wiki/StringRecipes
-    return e == '' or s:sub(-#e) == e
+--[[
+Try to match an array of patterns to a string.
+Parameters:
+  t .. Array of patterns (if nil, simply return false)
+  s .. A string
+Returns:
+  true if a pattern matched, otherwise false.
+]]--
+function M.match_any(t, s)
+  if not s then return false end
+  for _,p in pairs(t) do
+    if s:match(p) then return true end
+  end
+  return false
 end
 
-function M.is_higher_quality(a,b)
-    if a.height > b.height then
-        if a.width > b.width then
-            if a['bitrate'] then -- Optional: bitrate
-                if a.bitrate > b.bitrate then
-                    return true
-                end
-            else
-                return true
-            end
-        end
-    end
-    return false
+--[[
+Tokenize a string.
+Parameters:
+  s .. String to tokenize
+  p .. Pattern (e.g. "[%w-_]+")
+Returns:
+  An array of tokens.
+]]--
+function M.tokenize(s, p)
+  return s:gmatch(p)
 end
 
-function M.is_lower_quality(a,b)
-    if a.height < b.height then
-        if a.width < b.width then
-            if a['bitrate'] then -- Optiona: bitrate
-                if a.bitrate < b.bitrate then
-                    return true
-                end
-            else
-                return true
-            end
-        end
-    end
-    return false
-end
-
-function M.choose_format(self,
-                         formats,
-                         callback_choose_best,
-                         callback_choose_default,
-                         callback_to_s)
-    local r_fmt = self.requested_format
-
-    if r_fmt == 'best' then
-        return callback_choose_best(formats)
-    end
-
-    for _,v in pairs(formats) do
-        if r_fmt == callback_to_s(v) then
-            return v
-        end
-    end
-
-    return callback_choose_default(formats)
-end
-
+--[[
+Convert a string to a timestamp.
+Parameters:
+  s .. String to convert
+Returns:
+  Converted string.
+]]--
 function M.to_timestamp(s) -- Based on <http://is.gd/ee9ZTD>
-    local p = "%a+, (%d+) (%a+) (%d+) (%d+):(%d+):(%d+)"
+  local p = "%a+, (%d+) (%a+) (%d+) (%d+):(%d+):(%d+)"
 
-    local d,m,y,hh,mm,ss = s:match(p)
-    if not d then error('no match: date') end
+  local d,m,y,hh,mm,ss = s:match(p)
+  if not d then error('no match: date') end
 
-    local MON = {Jan=1, Feb=2, Mar=3, Apr=4, May=5, Jun=6, Jul=7, Aug=8,
-                 Sep=9, Oct=10, Nov=11, Dec=12}
+  local MON = {Jan=1, Feb=2, Mar=3, Apr=4, May=5, Jun=6, Jul=7, Aug=8,
+               Sep=9, Oct=10, Nov=11, Dec=12}
 
-    local m = MON[m]
+  local m = MON[m]
+  local offset = os.time() - os.time(os.date("!*t"))
 
-    local offset = os.time() - os.time(os.date("!*t"))
+  return os.time({day=d,month=m,year=y,
+                  hour=hh,min=mm,sec=ss}) + offset
+end
 
-    return os.time({day=d,month=m,year=y,
-                    hour=hh,min=mm,sec=ss}) + offset
+--[[
+Decode a string.
+Parameters:
+  s .. String to decode
+Returns:
+  Decoded string.
+]]--
+function M.decode(s) -- http://www.lua.org/pil/20.3.html
+  r = {}
+  for n,v in s:gmatch ("([^&=]+)=([^&=]+)") do
+    n = M.unescape(n)
+    r[n] = v
+  end
+  return r
+end
+
+--[[
+Unescape a string.
+Parameters:
+  s .. String to unescape
+Returns:
+  Unescaped string
+]]--
+function M.unescape(s) -- http://www.lua.org/pil/20.3.html
+  s = s:gsub('+', ' ')
+  return (s:gsub('%%(%x%x)',
+            function(h)
+              return string.char(tonumber(h, 16))
+            end))
+end
+
+--[[
+Unescape slashed string.
+Parameters:
+  s .. String to unescape
+Returns:
+  Unescaped string
+]]--
+function M.slash_unescape(s)
+  return (s:gsub('\\(.)', '%1'))
 end
 
 return M
 
--- vim: set ts=4 sw=4 tw=72 expandtab:
+-- vim: set ts=2 sw=2 tw=72 expandtab:
