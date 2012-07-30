f0bdfce073178b2afee79f672ffd4c0f692777b3
Toni Gundogdu <legatvs@gmail.com>
2012-07-30 20:43:16 +0300

    quvi/bit.lua: Apply new style
    Make minor changes to the code style.

--- share/lua/common/quvi/bit.lua
+++ share/lua/common/quvi/bit.lua
@@ -1,4 +1,3 @@
-
 -- libquvi-scripts
 -- Copyright (C) 2010  Toni Gundogdu <legatvs@gmail.com>
 --
@@ -22,28 +21,28 @@
 
 local M = {}
 
-function M.bit_or (x, y) -- http://is.gd/iVg4x
-    local p = 1
-    while p < x do p = p + p end
-    while p < y do p = p + p end
-    local z = 0
-    repeat
-        if p <= x or p <= y then
-            z = z + p
-            if p <= x then x = x - p end
-            if p <= y then y = y - p end
-        end
-        p = p * 0.5
-    until p < 1
-    return z
+function M.bit_or(x, y) -- http://is.gd/iVg4x
+  local p = 1
+  while p < x do p = p + p end
+  while p < y do p = p + p end
+  local z = 0
+  repeat
+    if p <= x or p <= y then
+      z = z + p
+      if p <= x then x = x - p end
+      if p <= y then y = y - p end
+    end
+    p = p * 0.5
+  until p < 1
+  return z
 end
 
 -- 1-based indexing
-function M.bit (p) return 2 ^ (p - 1) end
+function M.bit(p) return 2 ^ (p - 1) end
 
 -- e.g. "if has_bit (foo, bit (n)) then ..."
 function M.has_bit(x, p) return x % (p + p) >= p end
 
 return M
 
--- vim: set ts=4 sw=4 tw=72 expandtab:
+-- vim: set ts=2 sw=2 tw=72 expandtab:
