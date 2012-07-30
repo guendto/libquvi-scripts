feeb772e5db85b256a98eed9f0f524b36ad2d073
Toni Gundogdu <legatvs@gmail.com>
2012-07-30 23:18:19 +0300

    ident: Remove r.formats
    This has been deprecated since 0.4 .

--- share/lua/media/gaskrank.lua
+++ share/lua/media/gaskrank.lua
@@ -23,7 +23,6 @@
 function ident(self)
     local C      = require 'quvi/const'
     local r      = {}
-    r.formats    = "default"
     r.categories = C.proto_http
     local U      = require 'quvi/util'
     r.handles    = U.handles(self.page_url, {r.domain}, {"/tv/"})
