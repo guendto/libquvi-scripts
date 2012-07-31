002b5a8d589aa73cf8e8bdac39e3d88c521b9dc5
Toni Gundogdu <legatvs@gmail.com>
2012-07-31 12:35:34 +0300

    ident: Remove r.domain
    The library no longer expects this value.

--- share/lua/media/arte.lua
+++ share/lua/media/arte.lua
@@ -27,7 +27,6 @@ local Arte = {} -- Utility functions unique to to this script.
 function ident(qargs)
     local C      = require 'quvi/const'
     local r      = {}
-    r.domain     = "videos%.arte%.tv"
     r.formats    = "default|best"
     r.categories = C.proto_rtmp
     local U      = require 'quvi/util'
