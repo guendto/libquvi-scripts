52c7e5c23f893cee279608dc67981c39ab652cc9
Toni Gundogdu <legatvs@gmail.com>
2012-07-31 12:03:35 +0300

    ident: Remove r.domain
    The library no longer expects this value.

--- share/lua/media/dailymotion.lua
+++ share/lua/media/dailymotion.lua
@@ -28,7 +28,6 @@ local Dailymotion = {} -- Utility functions unique to this script.
 function ident(self)
     local C      = require 'quvi/const'
     local r      = {}
-    r.domain     = "dailymotion%.%w+"
     r.formats    = "default|best"
     r.categories = C.proto_http
     local U      = require 'quvi/util'
