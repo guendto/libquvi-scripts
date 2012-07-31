01b31375da628165e2794ecaec9a94d0a8c0c086
Toni Gundogdu <legatvs@gmail.com>
2012-07-31 13:26:12 +0300

    ident: Remove r.formats
    This value was deprecated in 0.4 and removed in 1.0 .

--- share/lua/media/vimeo.lua
+++ share/lua/media/vimeo.lua
@@ -29,7 +29,6 @@ local Vimeo = {} -- Utility functions unique to this script.
 function ident(qargs)
     local C      = require 'quvi/const'
     local r      = {}
-    r.formats    = "default|best"
     r.categories = C.proto_http
     local U      = require 'quvi/util'
     r.handles    = U.handles(self.page_url, {r.domain}, {"/%d+$"})
