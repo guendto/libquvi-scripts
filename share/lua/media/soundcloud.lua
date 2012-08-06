6946e06c5d0764026122941c088981525d10575b
Toni Gundogdu <legatvs@gmail.com>
2012-08-06 13:18:30 +0300

    Remove obsolete query_formats function
    The library no longer supports the "query formats" feature.

--- share/lua/media/soundcloud.lua
+++ share/lua/media/soundcloud.lua
@@ -33,12 +33,6 @@ function ident(qargs)
   return r
 end
 
--- Query available formats.
-function query_formats(self)
-    self.formats = 'default'
-    return self
-end
-
 -- Parse media URL.
 function parse(self)
     self.host_id = "soundcloud"
