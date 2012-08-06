dd428bbf6a38b327044b7218a044299ec8817d80
Toni Gundogdu <legatvs@gmail.com>
2012-08-06 11:41:33 +0300

    tests: Add media/arte.mk to media/tests.mk

--- tests/media/tests.mk
+++ tests/media/tests.mk
@@ -1,4 +1,5 @@
 include $(top_srcdir)/tests/media/academicearth.mk
+include $(top_srcdir)/tests/media/arte.mk
 include $(top_srcdir)/tests/media/break.mk
 include $(top_srcdir)/tests/media/dailymotion.mk
 include $(top_srcdir)/tests/media/gaskrank.mk
