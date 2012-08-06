75ba354f7ead86069189144a31858e55cb9f1fe1
Toni Gundogdu <legatvs@gmail.com>
2012-08-06 11:39:58 +0300

    tests: Add media/media_academicearth.c

---
+++ tests/media/media_academicearth.c
@@ -0,0 +1,61 @@
+/* libquvi-scripts
+ * Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/* media/academicearth.lua uses the "goto_url", this means that the media
+ * is hosted elsewhere, in this case: YouTube. */
+
+#include "config.h"
+
+#include <string.h>
+#include <glib.h>
+#include <quvi.h>
+
+#include "tests.h"
+
+static const gchar URL[] =
+  "http://www.academicearth.org/lectures/darwin-and-science";
+
+static const gchar TITLE[] =
+  "History 2D: Science, Magic, and Religion, Lecture 12, UCLA";
+
+static const gchar ID[] =
+  "QIDXKoy59Ho";
+
+static void test_media_academicearth()
+{
+  struct qm_test_exact_s e;
+  struct qm_test_opts_s o;
+
+  memset(&e, 0, sizeof(e));
+  memset(&o, 0, sizeof(o));
+
+  e.title = TITLE;
+  e.id = ID;
+
+  qm_test(__func__, URL, &e, &o);
+}
+
+gint main(gint argc, gchar **argv)
+{
+  g_test_init(&argc, &argv, NULL);
+  g_test_add_func("/media/academicearth", test_media_academicearth);
+  return (g_test_run());
+}
+
+/* vim: set ts=2 sw=2 tw=72 expandtab: */
