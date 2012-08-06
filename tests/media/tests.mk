faeb54f230893ccd9b1a3b90fee374f797f6af4c
Toni Gundogdu <legatvs@gmail.com>
2012-08-06 11:10:47 +0300

    tests: Add media/dailymotion.mk to media/tests.mk

--- tests/media/tests.mk
+++ tests/media/tests.mk
@@ -1,3 +1,4 @@
 include $(top_srcdir)/tests/media/break.mk
+include $(top_srcdir)/tests/media/dailymotion.mk
 include $(top_srcdir)/tests/media/gaskrank.mk
 include $(top_srcdir)/tests/media/youtube.mk
