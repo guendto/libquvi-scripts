65cb28f763b6d172dc506e1372334ad85e1f7cd4
Toni Gundogdu <legatvs@gmail.com>
2012-08-06 13:46:16 +0300

    tests: Add media/soundcloud.mk to media/tests.mk

--- tests/media/tests.mk
+++ tests/media/tests.mk
@@ -3,5 +3,6 @@ include $(top_srcdir)/tests/media/arte.mk
 include $(top_srcdir)/tests/media/break.mk
 include $(top_srcdir)/tests/media/dailymotion.mk
 include $(top_srcdir)/tests/media/gaskrank.mk
+include $(top_srcdir)/tests/media/soundcloud.mk
 include $(top_srcdir)/tests/media/vimeo.mk
 include $(top_srcdir)/tests/media/youtube.mk
