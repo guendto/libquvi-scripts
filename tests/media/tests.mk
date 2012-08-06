80660792f5f38c655c553ba944bfeb060e7e2c94
Toni Gundogdu <legatvs@gmail.com>
2012-08-06 11:19:04 +0300

    tests: Add media/vimeo.mk to media/tests.mk

--- tests/media/tests.mk
+++ tests/media/tests.mk
@@ -1,4 +1,5 @@
 include $(top_srcdir)/tests/media/break.mk
 include $(top_srcdir)/tests/media/dailymotion.mk
 include $(top_srcdir)/tests/media/gaskrank.mk
+include $(top_srcdir)/tests/media/vimeo.mk
 include $(top_srcdir)/tests/media/youtube.mk
