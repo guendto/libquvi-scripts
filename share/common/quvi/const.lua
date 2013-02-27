82d82f16ead030eb1fb93c21bdb8d993562c6ed2
Toni Gundogdu <legatvs@gmail.com>
2013-02-27 16:14:01 +0200

    quvi/const: Remove qoo_http_cookie

--- share/common/quvi/const.lua
+++ share/common/quvi/const.lua
@@ -51,7 +51,6 @@ M.qoo_croak_if_error      = 0x01 -- Terminate if an error occurs
 M.qoo_fetch_from_charset  = 0x20 -- Convert (to UTF-8) from this charset
 -- http
 M.qoo_http_user_agent     = 0x40 -- Identify to HTTP servers as
-M.qoo_http_cookie         = 0x41 -- Set an arbitrary cookie
 -- crypto
 M.qoo_crypto_cipher_flags = 0x60 -- Cipher flags
 M.qoo_crypto_cipher_mode  = 0x61 -- Cipher mode
