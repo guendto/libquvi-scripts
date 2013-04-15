4cb51d855f948bdb830ae45151f69c8a5a39734e
Toni Gundogdu <legatvs@gmail.com>
2013-04-15 21:15:48 +0300

    tests: lib: Add slist.c transl. unit
    Add slist_free_full to the library. This function provides a convenient
    way (for those who still use an earlier version of GLib) to release an
    entire singly-linked list. Normally we'd just use g_slist_free_full
    (since GLib 2.28).
    
    Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

--- tests/lib/tests.h
+++ tests/lib/tests.h
@@ -1,5 +1,5 @@
 /* libquvi-scripts
- * Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+ * Copyright (C) 2012,2013  Toni Gundogdu <legatvs@gmail.com>
  *
  * This file is part of libquvi-scripts <http://quvi.sourceforge.net>.
  *
@@ -23,13 +23,13 @@
 
 /* qerr.c */
 
-glong qerr(quvi_t);
+glong qerr(gpointer);
 
 /* env.c */
 
 gboolean chk_env(const gchar*, const gchar*);
 gboolean chk_skip(const gchar*);
-void chk_verbose(quvi_t);
+void chk_verbose(gpointer);
 gboolean chk_geoblocked();
 gboolean chk_complete();
 gboolean chk_fixme();
@@ -247,6 +247,8 @@ void qsub_test(const gchar*, const gchar*, qsub_test_opts_t);
 gboolean match(const gchar*, const gchar*);
 gchar *capture(const gchar*, const gchar*);
 gchar *fetch(const gchar*);
+
+void slist_free_full(GSList*, const GFunc);
 
 #endif /* tests_h */
 
