52a702f81c5fb4a6145fc2af590f8bef3eb75de2
Toni Gundogdu <legatvs@gmail.com>
2013-10-20 22:54:30 +0300

    tests: Add test for media/metacafe.lua
    Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

---
+++ tests/media/media_metacafe.c
@@ -0,0 +1,77 @@
+/* libquvi-scripts
+ * Copyright (C) 2013  Toni Gundogdu <legatvs@gmail.com>
+ *
+ * This file is part of libquvi-scripts <http://quvi.sourceforge.net>.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General
+ * Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <string.h>
+#include <glib.h>
+#include <quvi.h>
+
+#include "tests.h"
+
+static const gchar *URLs[] =
+{
+  "http://www.metacafe.com/watch/11082201/anthony_hopkins_breaking_bad_fan_letter/",
+  "http://www.metacafe.com/watch/10858110/kitten_refuses_to_return_money/",
+  NULL
+};
+
+static const gchar *TITLEs[] =
+{
+  "Anthony Hopkins' Breaking Bad Fan Letter",
+  "Kitten Refuses to Return Money",
+  NULL
+};
+
+static const gchar *IDs[] =
+{
+  "11082201",
+  "10858110",
+  NULL
+};
+
+static void test_media_metacafe()
+{
+  struct qm_test_exact_s e;
+  struct qm_test_opts_s o;
+  gint i;
+
+  for (i=0; URLs[i] != NULL; ++i)
+    {
+      memset(&e, 0, sizeof(e));
+      memset(&o, 0, sizeof(o));
+
+      e.title = TITLEs[i];
+      e.id = IDs[i];
+
+      o.gt0.duration_ms = TRUE;
+
+      qm_test(__func__, URLs[i], &e, &o);
+    }
+}
+
+gint main(gint argc, gchar **argv)
+{
+  g_test_init(&argc, &argv, NULL);
+  g_test_add_func("/media/metacafe", test_media_metacafe);
+  return (g_test_run());
+}
+
+/* vim: set ts=2 sw=2 tw=72 expandtab: */
