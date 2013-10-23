60da497acde322dad62c1d7b4c67603c302d89ae
Toni Gundogdu <legatvs@gmail.com>
2013-10-23 21:43:54 +0300

    media/tagtele.lua: Rewrite ident function
    Rewrite the `ident' function for the libquvi 0.9+ series.
    * Implement `Tagtele.can_parse_url' function
    * Remove any now obsolete code
    
    Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

--- share/media/tagtele.lua
+++ share/media/tagtele.lua
@@ -1,5 +1,5 @@
 -- libquvi-scripts
--- Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+-- Copyright (C) 2012,2013  Toni Gundogdu <legatvs@gmail.com>
 -- Copyright (C) 2010  Paul Kocialkowski <contact@paulk.fr>
 --
 -- This file is part of libquvi-scripts <http://quvi.sourceforge.net/>.
@@ -19,17 +19,14 @@
 -- <http://www.gnu.org/licenses/>.
 --
 
--- Identify the script.
-function ident(self)
-    package.path = self.script_dir .. '/?.lua'
-    local C      = require 'quvi/const'
-    local r      = {}
-    r.domain     = "tagtele%.com"
-    r.formats    = "default"
-    r.categories = C.proto_http
-    local U      = require 'quvi/util'
-    r.handles    = U.handles(self.page_url, {r.domain}, {"/videos/voir/%d+"})
-    return r
+local Tagtele = {} -- Utility functions unique to this script
+
+-- Identify the media script.
+function ident(qargs)
+  return {
+    can_parse_url = Tagtele.can_parse_url(qargs),
+    domains = table.concat({'tagtele.com'}, ',')
+  }
 end
 
 -- Query available formats.
@@ -57,6 +54,23 @@ function parse(self)
                   or error("no match: media URL")}
 
     return self
+end
+
+--
+-- Utility functions.
+--
+
+function Tagtele.can_parse_url(qargs)
+  local U = require 'socket.url'
+  local t = U.parse(qargs.input_url)
+  if t and t.scheme and t.scheme:lower():match('^https?$')
+       and t.host   and t.host:lower():match('^www%.tagtele%.com$')
+       and t.path   and t.path:lower():match('^/videos/voir/%d+/$')
+  then
+    return true
+  else
+    return false
+  end
 end
 
 -- vim: set ts=4 sw=4 tw=72 expandtab:
