c8e7924c31d47ff00630a823a85638580699801d
Toni Gundogdu <legatvs@gmail.com>
2013-11-10 12:20:28 +0200

    media/collegehumor.lua: Rewrite parse function
    Rewrite the `parse' function for the libquvi 0.9+ series.
    * Remove now obsolete code (e.g. self.host_id)
    * Apply 2-space indentation
    * Update vim modeline
    
    Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

--- share/media/collegehumor.lua
+++ share/media/collegehumor.lua
@@ -29,30 +29,32 @@ function ident(qargs)
   }
 end
 
--- Parse media URL.
-function parse(self)
-    self.host_id  = "collegehumor"
+-- Parse the media properties.
+function parse(qargs)
+  local p = quvi.http.fetch(qargs.input_url).data
 
-    if CollegeHumor.redirect_if_embed(self) then
-        return self
-    end
+  local u = p:match('source type=.- src="(.-)"')
+              or error('no match: media stream URL')
 
-    local c = CollegeHumor.get_config(self)
+  if u:match('%.%w+$') then -- Stream URL ends to a file extension?
+    local S = require 'quvi/stream'
+    qargs.streams = {S.stream_new(u)}
+  else
+    qargs.goto_url = u -- Affiliate content.
+    return qargs       -- Pass the new page URL back to the library.
+  end
 
-    self.title = c:match('<caption>.-%[.-%[(.-)%]%]')
-                  or error("no match: media title")
+  qargs.duration_ms =
+    tonumber(p:match('"video:duration" content="(%d+)"') or 0) *1000
 
-    self.thumbnail_url = c:match('<thumbnail><!%[.-%[(.-)%]') or ''
+  qargs.thumb_url = p:match('"og:image" content="(.-)"') or ''
 
-    local formats = CollegeHumor.iter_formats(c)
-    local U       = require 'quvi/util'
-    local format  = U.choose_format(self, formats,
-                                     CollegeHumor.choose_best,
-                                     CollegeHumor.choose_default,
-                                     CollegeHumor.to_s)
-                        or error("unable to choose format")
-    self.url      = {format.url or error("no match: media URL")}
-    return self
+  qargs.title = p:match('"og:title" content="(.-)"') or ''
+
+  qargs.id = qargs.input_url:match('/video/(%d+)/')
+                or qargs.input_url:match('/embed/(%d+)/') or ''
+
+  return qargs
 end
 
 --
@@ -73,76 +75,4 @@ function CollegeHumor.can_parse_url(qargs)
   end
 end
 
-function CollegeHumor.redirect_if_embed(self) -- dorkly embeds YouTube videos
-    if self.page_url:match('/embed/%d+') then
-        local p = quvi.fetch(self.page_url)
-        local s = p:match('youtube.com/embed/([%w-_]+)')
-        if s then
-            -- Hand over to youtube.lua
-            self.redirect_url = 'http://youtube.com/watch?v=' .. s
-            return true
-        end
-    end
-    return false
-end
-
-function CollegeHumor.get_media_id(self)
-    local U = require 'quvi/url'
-    local domain = U.parse(self.page_url).host:gsub('^www%.', '', 1)
-
-    self.host_id = domain:match('^(.+)%.[^.]+$') or error("no match: domain")
-
-    self.id = self.page_url:match('/video[/:](%d+)')
-                or error("no match: media ID")
-
-    return domain
-end
-
-function CollegeHumor.get_config(self)
-    local domain = CollegeHumor.get_media_id(self)
-
-    -- quvi normally checks the page URL for a redirection to another
-    -- URL. Disabling this check (QUVIOPT_NORESOLVE) breaks the support
-    -- which is why we do this manually here.
-    local r = quvi.resolve(self.page_url)
-
-    -- Make a note of the use of the quvi.resolve returned string.
-    local u = string.format("http://www.%s/moogaloop/video%s%s",
-                              domain, (#r > 0) and ':' or '/', self.id)
-
-    return quvi.fetch(u, {fetch_type='config'})
-end
-
-function CollegeHumor.iter_formats(config)
-    local sd_url = config:match('<file><!%[.-%[(.-)%]')
-    local hq_url = config:match('<hq><!%[.-%[(.-)%]')
-    local hq_avail = (hq_url and #hq_url > 0) and 1 or 0
-
-    local t = {}
-
-    local s = sd_url:match('%.(%w+)$')
-    table.insert(t, {quality='sd', url=sd_url, container=s})
-
-    if hq_avail == 1 and hq_url then
-        local s = hq_url:match('%.(%w+)$')
-        table.insert(t, {quality='hq', url=hq_url, container=s})
-    end
-
-    return t
-end
-
-function CollegeHumor.choose_best(formats) -- Assume last is best.
-    local r
-    for _,v in pairs(formats) do r = v end
-    return r
-end
-
-function CollegeHumor.choose_default(formats) -- Whatever is found first.
-    for _,v in pairs(formats) do return v end
-end
-
-function CollegeHumor.to_s(t)
-    return string.format('%s_%s', t.container, t.quality)
-end
-
--- vim: set ts=4 sw=4 tw=72 expandtab:
+-- vim: set ts=2 sw=2 tw=72 expandtab:
